b5d5cd02c0a13eed35e75fa91809137fa85bd482
Misbah Baqir <MisbahBaqir>
2023-11-11 02:43:57 +0500

    PHP

---
+++ filehandling.php
@@ -0,0 +1,239 @@
+<?php
+
+//$page = readfile ("Seerat of Hazrat Muhammad (S.A.W).txt");
+// echo $page; 
+
+$fileopen = fopen("Seerat of Hazrat Muhammad (S.A.W).txt" , "r");
+
+if(!$fileopen) {
+    die ("failed to open file");
+}
+
+while ($a = fgetc($fileopen)) {
+    echo  fgets($fileopen) ;
+    echo $a;
+    echo "<br>";
+
+}
+
+fclose($fileopen);
+
+//$page = readfile ("Hazrat Khadija (R.A).txt");
+ //echo $page; 
+
+$fileopen = fopen("Hazrat Khadija (R.A).txt" , "r");
+
+if(!$fileopen) {
+    die ("failed to open file");
+}
+
+while ($a = fgetc($fileopen)) {
+    echo  fgets($fileopen) ;
+    echo $a;
+    echo "<br>";
+
+}
+
+fclose($fileopen);
+
+
+
+ //$page = readfile ("Hazrat Aisha (R.A).txt");
+ //echo $page; 
+
+ $fileopen = fopen("Hazrat Aisha (R.A).txt" , "r");
+
+ if(!$fileopen) {
+     die ("failed to open file");
+ }
+ 
+ while ($a = fgetc($fileopen)) {
+     echo  fgets($fileopen) ;
+     echo $a;
+     echo "<br>";
+ 
+ }
+ 
+fclose($fileopen);
+
+//$page = readfile ("Hazrat Abu Bakar (R.A).txt");
+ //echo $page; 
+
+ $fileopen = fopen("Hazrat Abu Bakar (R.A).txt" , "r");
+
+ if(!$fileopen) {
+     die ("failed to open file");
+ }
+ 
+ while ($a = fgetc($fileopen)) {
+     echo  fgets($fileopen) ;
+     echo $a;
+     echo "<br>";
+ 
+ }
+ 
+fclose($fileopen);
+
+// $page = readfile ("Hazrat Umer Ibn Al-Khattab (R.A).txt");
+ //echo $page; 
+
+ $fileopen = fopen("Hazrat Umer Ibn Al-Khattab (R.A).txt" , "r");
+
+ if(!$fileopen) {
+     die ("failed to open file");
+ }
+ 
+ while ($a = fgetc($fileopen)) {
+     echo  fgets($fileopen) ;
+     echo $a;
+     echo "<br>";
+ 
+}
+ 
+fclose($fileopen);
+
+  //$page = readfile ("Hazrat Usman Bin Affan(R.A).txt");
+ //echo $page;
+ 
+ $fileopen = fopen("Hazrat Usman Bin Affan(R.A).txt" , "r");
+
+ if(!$fileopen) {
+     die ("failed to open file");
+ }
+ 
+ while ($a = fgetc($fileopen)) {
+     echo  fgets($fileopen) ;
+     echo $a;
+     echo "<br>";
+ 
+}
+ 
+ fclose($fileopen);
+
+ //$page = readfile ("Hazrat Ali Ibn Abi Talib(R.A).txt");
+ //echo $page;
+ 
+ $fileopen = fopen("Hazrat Ali Ibn Abi Talib(R.A).txt" , "r");
+
+ if(!$fileopen) {
+     die ("failed to open file");
+ }
+ 
+ while ($a = fgetc($fileopen)) {
+     echo  fgets($fileopen) ;
+     echo $a;
+     echo "<br>";
+ 
+}
+ 
+ fclose($fileopen);
+
+ //$page = readfile ("Hazrat Talha Ibn Ubaidullah (R.A).txt");
+ //echo $page;
+ 
+ $fileopen = fopen("Hazrat Talha Ibn Ubaidullah (R.A).txt" , "r");
+
+ if(!$fileopen) {
+     die ("failed to open file");
+ }
+ 
+ while ($a = fgetc($fileopen)) {
+     echo  fgets($fileopen) ;
+     echo $a;
+     echo "<br>";
+ 
+}
+ 
+ fclose($fileopen);
+
+ //$page = readfile ("Hazrat Zubair Ibn Al-Awwam (R.A).txt");
+ //echo $page;
+ 
+ $fileopen = fopen("Hazrat Zubair Ibn Al-Awwam (R.A).txt" , "r");
+
+ if(!$fileopen) {
+     die ("failed to open file");
+ }
+ 
+ while ($a = fgetc($fileopen)) {
+     echo  fgets($fileopen) ;
+     echo $a;
+     echo "<br>";
+ 
+}
+ 
+ fclose($fileopen);
+
+ //$page = readfile ("Hazrat Abdur Rehman Ibn Awf (R.A).txt");
+ //echo $page;
+ 
+ $fileopen = fopen("Hazrat Abdur Rehman Ibn Awf (R.A).txt" , "r");
+
+ if(!$fileopen) {
+     die ("failed to open file");
+ }
+ 
+ while ($a = fgetc($fileopen)) {
+     echo  fgets($fileopen) ;
+     echo $a;
+     echo "<br>";
+ 
+}
+ 
+ fclose($fileopen);
+
+ //$page = readfile ("Hazrat Saad Ibn Abi Waqas (R.A).txt");
+ //echo $page;
+ 
+ $fileopen = fopen("Hazrat Saad Ibn Abi Waqas (R.A).txt" , "r");
+
+ if(!$fileopen) {
+     die ("failed to open file");
+ }
+ 
+ while ($a = fgetc($fileopen)) {
+     echo  fgets($fileopen) ;
+     echo $a;
+     echo "<br>";
+ 
+}
+ 
+ fclose($fileopen);
+
+ //$page = readfile ("Hazrat Saeed Bin Zaid (R.A).txt");
+ //echo $page;
+ 
+ $fileopen = fopen("Hazrat Saeed Bin Zaid (R.A).txt" , "r");
+
+ if(!$fileopen) {
+     die ("failed to open file");
+ }
+ 
+ while ($a = fgetc($fileopen)) {
+     echo  fgets($fileopen) ;
+     echo $a;
+     echo "<br>";
+ 
+}
+ 
+ fclose($fileopen);
+
+ //$page = readfile ("Hazrat Abu Ubaidah Ibn Al-Jarrah (R.A).txt");
+ //echo $page;
+ 
+ $fileopen = fopen("Hazrat Abu Ubaidah Ibn Al-Jarrah (R.A).txt" , "r");
+
+ if(!$fileopen) {
+     die ("failed to open file");
+ }
+ 
+ while ($a = fgetc($fileopen)) {
+     echo  fgets($fileopen) ;
+     echo $a;
+     echo "<br>";
+ 
+}
+ 
+ fclose($fileopen);
+ 
+?>
